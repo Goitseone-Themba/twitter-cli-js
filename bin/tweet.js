9934864a4f747dfbd577ec2b8fc16625cb71882e
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-05-22 12:23:13 +0200

    use v2 for tweet

--- bin/tweet.js
+++ bin/tweet.js
@@ -9,14 +9,17 @@ program
   .argument('<message>', 'the tweet message to send')
   .action(async (message) => {
     try {
+      // Validate tweet length (Twitter's limit is 280 characters)
       if (message.length > 280) {
         console.error('Error: Tweet exceeds 280 characters.');
         process.exit(1);
       }
 
+      // Verify authentication
       const user = await twitterClient.v2.me();
       console.log(`Authenticated as @${user.data.username}`);
 
+      // Send the tweet using v2 API
       const tweet = await twitterClient.v2.tweet(message);
       console.log('Tweet sent successfully!');
       console.log(`https://twitter.com/${user.data.username}/status/${tweet.data.id}`);
