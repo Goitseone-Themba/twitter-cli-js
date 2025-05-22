e6febefc38a8d943409788b1d918b53ac8348119
Goitseone-Themba <goitseonerozthemba@gmail.com>
2025-05-22 11:32:36 +0200

    feat: add auth and tweet

---
+++ bin/tweet.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const { program } = require('commander');
+const twitterClient = require('../lib/auth');
+
+program
+  .version('0.0.1')
+  .description('A CLI tool to send tweets from your terminal (JavaScript Edition)')
+  .argument('<message>', 'the tweet message to send')
+  .action(async (message) => {
+    try {
+      if (message.length > 280) {
+        console.error('Error: Tweet exceeds 280 characters.');
+        process.exit(1);
+      }
+
+      const user = await twitterClient.v2.me();
+      console.log(`Authenticated as @${user.data.username}`);
+
+      const tweet = await twitterClient.v2.tweet(message);
+      console.log('Tweet sent successfully!');
+      console.log(`https://twitter.com/${user.data.username}/status/${tweet.data.id}`);
+    } catch (error) {
+      console.error('Error:', error.message || error);
+      if (error.data) {
+        console.error('API Error Details:', error.data);
+      }
+      process.exit(1);
+    }
+  });
+
+program.showHelpAfterError('(add --help for additional information)');
+
+program.parse(process.argv);
